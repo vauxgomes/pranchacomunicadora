1069db4f6c302ca2cd220e134c1af55c40d6f13e
Vaux Gomes <vauxsandino@gmail.com>
2021-10-22 14:17:32 -0300

    - Bugfix: Voice choosing: Printting steps on console.

--- js/texttospeech.js
+++ js/texttospeech.js
@@ -7,9 +7,12 @@ class TextToSpeech {
 		this.speech.volume = 1;
 
 		let choosingVoice = setInterval(() => {
+			console.log('Trying to get Voices');
 			let voices = window.speechSynthesis.getVoices();
 
 			if (voices && voices.length > 0) {
+				console.log('Choosing a voice');
+
 				this.speech.voice = voices.filter((voice) => {
 					return (
 						voice.name.toLowerCase().includes('português') ||
@@ -21,13 +24,11 @@ class TextToSpeech {
 					this.speech.voice = window.speechSynthesis.getVoices()[14];
 				}
 
+				console.log(`Chosen voice: ${this.speech.voice}`);
 				clearInterval(choosingVoice);
 
-				try {
-					alert(this.speech.voice.name);
-				} catch (error) {
-					alert('Erro')
-				}
+				if (this.speech.voice) alert(this.speech.voice.name);
+				else alert('Erro choosing voice');
 			}
 		}, 10);
 	}
