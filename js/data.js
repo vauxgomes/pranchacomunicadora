8942f6346f35015b226b66e4b018a4f3aa5d4488
Vaux Gomes <vauxsandino@gmail.com>
2021-10-23 11:49:15 -0300

    - Populating items from data file - Menu classed adjusted - Page styles removed

---
+++ js/data.js
@@ -0,0 +1,144 @@
+let data = [
+	{
+		termos: [
+			{
+				text: 'Eu',
+				src: 'https://i.picsum.photos/id/896/200/200.jpg?hmac=GtnSTSOmlmBRvu2fpraj0-9azTBV0t32V07JLyAVPhM',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Você',
+				src: 'https://i.picsum.photos/id/353/200/200.jpg?hmac=0pI-jYBxEC3AHp_0G8YowhiQRtQdv6u1Kfvuf0c9VNQ',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ele/Ela',
+				src: 'https://i.picsum.photos/id/667/200/200.jpg?hmac=Dqc51PnEPXpiiStRcDoPxytal0MOGvzg-eDZ4BsVIz8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Olá',
+				src: 'https://i.picsum.photos/id/16/200/200.jpg?hmac=RGkrMlBKS58bjVd__gfGtWpmouvlXbzUHDjGHNcbIic',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Tudo bem',
+				src: 'https://i.picsum.photos/id/1051/200/200.jpg?hmac=s6d4ypEjpec8nvA2zqhWzx_6ogXYM2fJ_YJwaOM1CUA',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Como vai',
+				src: 'https://i.picsum.photos/id/1060/200/200.jpg?hmac=M0E6SK-_reDe8rAPtwDpww5ihTgL6yewgERGc7eX5z8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Estou bem',
+				src: 'https://i.picsum.photos/id/336/200/200.jpg?hmac=VZ7MzNM30jINYNf5Oj_8zqPLTDAyKDk6eXWTGnNb4bU',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Que bom te ver',
+				src: 'https://i.picsum.photos/id/147/200/200.jpg?hmac=Wk9sBW0wWCdCCp0HIczINX1TUZ_paAR_tCIKXcuYf2k',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Juntos',
+				src: 'https://i.picsum.photos/id/816/200/200.jpg?hmac=ZNsj0gw4AJtSDA82gMxujRjh3_5ZHoVje8CtZopmcz4',
+				alt: 'Lorem Picsum',
+			},
+		],
+	},
+	{
+		termos: [
+			{
+				text: 'Querer',
+				src: 'https://i.picsum.photos/id/760/200/200.jpg?hmac=MlH81fLiPAKVmDN5qADWkd_299jLg9vp9uVnRn3uxp8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Gostar',
+				src: 'https://i.picsum.photos/id/591/200/200.jpg?hmac=5agpVWsRchY0DObXs23vYWjjgqLZEBhqSvTwfCAcyng',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Estar',
+				src: 'https://i.picsum.photos/id/650/200/200.jpg?hmac=gu3C13pBxCSHokbnumczMYlmWRLt3CFGx1sDaPpfRnk',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ir',
+				src: 'https://i.picsum.photos/id/565/200/200.jpg?hmac=QvKo8qgzFFNcZoXCpT0CNMDTwWd3ynwqLXxrzK2o8fw',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Parar',
+				src: 'https://i.picsum.photos/id/102/200/200.jpg?hmac=mLVQr0-wbZ-PRSf_R4oFkUDR7yfIKqT3F8pfJNFZKc0',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ser',
+				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ficar',
+				src: 'https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Sentir',
+				src: 'https://i.picsum.photos/id/51/200/200.jpg?hmac=AxDMciMBjL8UIKzxl80paiBUywP7elWptqQW_qTq8vw',
+				alt: 'Lorem Picsum',
+			},
+		],
+	},
+	{
+		termos: [
+			{
+				text: 'Mãe',
+				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Água',
+				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Comida',
+				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Banheiro',
+				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Frio',
+				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Calor',
+				src: 'https://i.picsum.photos/id/1080/200/200.jpg?hmac=0okKAdyiW9oTgR5PNZQrDYFtWu7HAt93nI93ZpfelUw',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Amigo',
+				src: 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Bom',
+				src: 'https://i.picsum.photos/id/638/200/200.jpg?hmac=64UpQ4ouFUNEG9cnXLQ9GxchDShg-mL1rdCrZGfc94U',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ruim',
+				src: 'https://i.picsum.photos/id/268/200/200.jpg?hmac=I5JAWzISJc5x0jlDhEngvCIwyM0zxRh22iIIzHqOT80',
+				alt: 'Lorem Picsum',
+			},
+		],
+	},
+];
