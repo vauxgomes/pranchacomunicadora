2d3a8e3e0c35bcea8d525292f549bb0d1841b899
Vaux Gomes <vauxsandino@gmail.com>
2021-10-23 14:51:49 -0300

    - New styles hierarchy - New example images - New data blocks - Now using colors - Orders added to data file

--- js/data.js
+++ js/data.js
@@ -1,6 +1,10 @@
 let data = [
 	{
-		termos: [
+		id: 1635008399217,
+		order: 0,
+		name: 'Pronomes',
+		color: '#06d6a0',
+		items: [
 			{
 				text: 'Eu',
 				src: 'https://i.picsum.photos/id/896/200/200.jpg?hmac=GtnSTSOmlmBRvu2fpraj0-9azTBV0t32V07JLyAVPhM',
@@ -12,131 +16,200 @@ let data = [
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Ele/Ela',
+				text: 'Ele',
 				src: 'https://i.picsum.photos/id/667/200/200.jpg?hmac=Dqc51PnEPXpiiStRcDoPxytal0MOGvzg-eDZ4BsVIz8',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Olá',
+				text: 'Ela',
 				src: 'https://i.picsum.photos/id/16/200/200.jpg?hmac=RGkrMlBKS58bjVd__gfGtWpmouvlXbzUHDjGHNcbIic',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Tudo bem',
+				text: 'Nós',
 				src: 'https://i.picsum.photos/id/1051/200/200.jpg?hmac=s6d4ypEjpec8nvA2zqhWzx_6ogXYM2fJ_YJwaOM1CUA',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Como vai',
+				text: 'Isto',
 				src: 'https://i.picsum.photos/id/1060/200/200.jpg?hmac=M0E6SK-_reDe8rAPtwDpww5ihTgL6yewgERGc7eX5z8',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Estou bem',
+				text: 'Meu',
 				src: 'https://i.picsum.photos/id/336/200/200.jpg?hmac=VZ7MzNM30jINYNf5Oj_8zqPLTDAyKDk6eXWTGnNb4bU',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Que bom te ver',
+				text: 'Seu',
 				src: 'https://i.picsum.photos/id/147/200/200.jpg?hmac=Wk9sBW0wWCdCCp0HIczINX1TUZ_paAR_tCIKXcuYf2k',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Juntos',
-				src: 'https://i.picsum.photos/id/816/200/200.jpg?hmac=ZNsj0gw4AJtSDA82gMxujRjh3_5ZHoVje8CtZopmcz4',
 				alt: 'Lorem Picsum',
 			},
 		],
 	},
 	{
-		termos: [
+		id: 1635008786015,
+		order: 1,
+		name: 'Perguntas',
+		color: '#ffd166',
+		items: [
 			{
-				text: 'Querer',
+				text: 'O que?',
 				src: 'https://i.picsum.photos/id/760/200/200.jpg?hmac=MlH81fLiPAKVmDN5qADWkd_299jLg9vp9uVnRn3uxp8',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Gostar',
+				text: 'Quem?',
 				src: 'https://i.picsum.photos/id/591/200/200.jpg?hmac=5agpVWsRchY0DObXs23vYWjjgqLZEBhqSvTwfCAcyng',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Estar',
+				text: 'Onde?',
 				src: 'https://i.picsum.photos/id/650/200/200.jpg?hmac=gu3C13pBxCSHokbnumczMYlmWRLt3CFGx1sDaPpfRnk',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Ir',
+				text: 'Quando?',
 				src: 'https://i.picsum.photos/id/565/200/200.jpg?hmac=QvKo8qgzFFNcZoXCpT0CNMDTwWd3ynwqLXxrzK2o8fw',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Parar',
+				text: 'Como?',
 				src: 'https://i.picsum.photos/id/102/200/200.jpg?hmac=mLVQr0-wbZ-PRSf_R4oFkUDR7yfIKqT3F8pfJNFZKc0',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Ser',
+				text: 'Qual?',
 				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Ficar',
+				text: 'Quantos?',
 				src: 'https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Sentir',
-				src: 'https://i.picsum.photos/id/51/200/200.jpg?hmac=AxDMciMBjL8UIKzxl80paiBUywP7elWptqQW_qTq8vw',
+				text: 'Que horas?',
+				src: 'https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8',
 				alt: 'Lorem Picsum',
 			},
 		],
 	},
 	{
-		termos: [
+		id: 1635008786015,
+		order: 2,
+		name: 'Verbos I',
+		color: '#118ab2',
+		items: [
 			{
-				text: 'Mãe',
+				text: 'Estar',
 				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Água',
+				text: 'Querer',
 				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Comida',
+				text: 'Precisar',
 				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Banheiro',
+				text: 'Poder',
 				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Frio',
+				text: 'Gostar',
 				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Calor',
+				text: 'Saber',
 				src: 'https://i.picsum.photos/id/1080/200/200.jpg?hmac=0okKAdyiW9oTgR5PNZQrDYFtWu7HAt93nI93ZpfelUw',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Amigo',
+				text: 'Fazer',
 				src: 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Bom',
+				text: 'Ler',
 				src: 'https://i.picsum.photos/id/638/200/200.jpg?hmac=64UpQ4ouFUNEG9cnXLQ9GxchDShg-mL1rdCrZGfc94U',
 				alt: 'Lorem Picsum',
 			},
 			{
-				text: 'Ruim',
+				text: 'Tentar',
 				src: 'https://i.picsum.photos/id/268/200/200.jpg?hmac=I5JAWzISJc5x0jlDhEngvCIwyM0zxRh22iIIzHqOT80',
+				alt: 'Lorem Picsum',
+			},
+		],
+	},
+	{
+		id: 1635010742853,
+		order: 3,
+		name: 'Verbos II',
+		color: '#ef476f',
+		items: [
+			{
+				text: 'Comer',
+				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Beber',
+				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Ir',
+				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Vir',
+				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Sentir',
+				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
+				alt: 'Lorem Picsum',
+			},
+		],
+	},
+	{
+		id: 1635011064304,
+		order: 3,
+		name: 'Outros',
+		color: '#fb5607',
+		items: [
+			{
+				text: 'Pai',
+				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Mãe',
+				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Flamengo',
+				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Obrigado',
+				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
+				alt: 'Lorem Picsum',
+			},
+			{
+				text: 'Por favor',
+				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
 				alt: 'Lorem Picsum',
 			},
 		],
