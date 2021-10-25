9daa37bdab13bc5e578936281b981055cbbef202
vaux.gomes <vauxsandino@gmail.com>
2021-10-25 15:20:32 -0300

    - New icons from  Flat Icon

--- js/data.js
+++ js/data.js
@@ -1,217 +1,245 @@
 let data = [
-	{
-		id: 1635008399217,
-		order: 0,
-		name: 'Pronomes',
-		color: '#06d6a0',
-		items: [
+    {
+        id: 1635008399217,
+        order: 0,
+        name: "Pronomes",
+        color: "#06d6a0",
+        items: [
+            {
+                text: "Eu",
+                src: "https://cdn-icons-png.flaticon.com/512/5737/5737070.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Você",
+                src: "https://cdn-icons-png.flaticon.com/512/544/544507.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Ele",
+                src: "https://cdn-icons-png.flaticon.com/512/5294/5294576.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Ela",
+                src: "https://cdn-icons-png.flaticon.com/512/5294/5294584.png",
+                alt: "Flar Icon",
+            },
+            {
+                text: "Nós",
+                src: "https://cdn-icons-png.flaticon.com/512/5205/5205203.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Isto",
+                src: "https://cdn-icons-png.flaticon.com/512/2235/2235335.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Meu",
+                src: "https://cdn-icons-png.flaticon.com/512/5799/5799109.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Seu",
+                src: "https://cdn-icons-png.flaticon.com/512/5799/5799098.png",
+                alt: "Flat Icon",
+            },
+        ],
+    },
+    {
+        id: 1635008786015,
+        order: 1,
+        name: "Perguntas",
+        color: "#ffd166",
+        items: [
+            {
+                text: "O que?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219829.png?token=exp=1635183982~hmac=9d059037747cd2a1b4c865115c7ad24b",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Quem?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219621.png?token=exp=1635183988~hmac=e8c0cfb4ec3d09164df245252c00b5db",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Onde?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219808.png?token=exp=1635184070~hmac=310b3111e4efc68e5c68fcb52b0e0ad9",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Quando?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219766.png?token=exp=1635184249~hmac=480485e9ce7d348e63266cac79cc5512",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Como?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219818.png?token=exp=1635184112~hmac=6793a2b50d1d169691950e91a2ad16f1",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Qual?",
+                src: "https://cdn-icons.flaticon.com/png/512/4219/premium/4219852.png?token=exp=1635184166~hmac=68142ab957e9cc8feb8b419d443025a1",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Quantos?",
+                src: "https://cdn-icons-png.flaticon.com/512/4219/4219587.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Que horas?",
+                src: "https://cdn-icons-png.flaticon.com/512/5346/5346162.png",
+                alt: "Flat Icon",
+            },
+        ],
+    },
+    {
+        id: 1635008786015,
+        order: 2,
+        name: "Verbos I",
+        color: "#118ab2",
+        items: [
+            {
+                text: "Estar",
+                src: "https://cdn-icons-png.flaticon.com/512/5737/5737025.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Querer",
+                src: "https://cdn-icons-png.flaticon.com/512/1107/1107460.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Precisar",
+                src: "https://cdn-icons.flaticon.com/png/512/3038/premium/3038235.png?token=exp=1635184766~hmac=a71534be5b7b6a1b7509fa4746bc0233",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Poder",
+                src: "https://cdn-icons-png.flaticon.com/512/5737/5737169.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Gostar",
+                src: "https://cdn-icons-png.flaticon.com/512/5906/5906018.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Saber",
+                src: "https://cdn-icons-png.flaticon.com/512/5201/5201967.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Fazer",
+                src: "https://cdn-icons-png.flaticon.com/512/820/820723.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Ler",
+                src: "https://cdn-icons-png.flaticon.com/512/3153/3153339.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Tentar",
+                src: "https://cdn-icons-png.flaticon.com/512/5737/5737221.png",
+                alt: "Flat Icon",
+            },
+        ],
+    },
+    {
+        id: 1635010742853,
+        order: 3,
+        name: "Verbos II",
+        color: "#ef476f",
+        items: [
+            {
+                text: "Comer",
+                src: "https://cdn-icons-png.flaticon.com/512/5757/5757041.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Beber",
+                src: "https://cdn-icons-png.flaticon.com/512/5019/5019733.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Ir",
+                src: "https://cdn-icons-png.flaticon.com/512/5019/5019577.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Vir",
+                src: "https://cdn-icons.flaticon.com/png/512/4846/premium/4846170.png?token=exp=1635186620~hmac=872c4d33a0c40c058e4dcd9342a42d09",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Sentir",
+                src: "https://cdn-icons-png.flaticon.com/512/5737/5737014.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Estudar",
+                src: "https://cdn-icons.flaticon.com/png/512/3153/premium/3153467.png?token=exp=1635185562~hmac=c979b03abaaad19ff63bb365dc878f05",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Passear",
+                src: "https://cdn-icons.flaticon.com/png/512/3153/premium/3153346.png?token=exp=1635185562~hmac=78cfda411766b58d4ded411586d5a598",
+				alt: "Flat Icon",
+            },
 			{
-				text: 'Eu',
-				src: 'https://i.picsum.photos/id/896/200/200.jpg?hmac=GtnSTSOmlmBRvu2fpraj0-9azTBV0t32V07JLyAVPhM',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Você',
-				src: 'https://i.picsum.photos/id/353/200/200.jpg?hmac=0pI-jYBxEC3AHp_0G8YowhiQRtQdv6u1Kfvuf0c9VNQ',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Ele',
-				src: 'https://i.picsum.photos/id/667/200/200.jpg?hmac=Dqc51PnEPXpiiStRcDoPxytal0MOGvzg-eDZ4BsVIz8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Ela',
-				src: 'https://i.picsum.photos/id/16/200/200.jpg?hmac=RGkrMlBKS58bjVd__gfGtWpmouvlXbzUHDjGHNcbIic',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Nós',
-				src: 'https://i.picsum.photos/id/1051/200/200.jpg?hmac=s6d4ypEjpec8nvA2zqhWzx_6ogXYM2fJ_YJwaOM1CUA',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Isto',
-				src: 'https://i.picsum.photos/id/1060/200/200.jpg?hmac=M0E6SK-_reDe8rAPtwDpww5ihTgL6yewgERGc7eX5z8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Meu',
-				src: 'https://i.picsum.photos/id/336/200/200.jpg?hmac=VZ7MzNM30jINYNf5Oj_8zqPLTDAyKDk6eXWTGnNb4bU',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Seu',
-				src: 'https://i.picsum.photos/id/147/200/200.jpg?hmac=Wk9sBW0wWCdCCp0HIczINX1TUZ_paAR_tCIKXcuYf2k',
-				alt: 'Lorem Picsum',
-			},
-		],
-	},
-	{
-		id: 1635008786015,
-		order: 1,
-		name: 'Perguntas',
-		color: '#ffd166',
-		items: [
-			{
-				text: 'O que?',
-				src: 'https://i.picsum.photos/id/760/200/200.jpg?hmac=MlH81fLiPAKVmDN5qADWkd_299jLg9vp9uVnRn3uxp8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Quem?',
-				src: 'https://i.picsum.photos/id/591/200/200.jpg?hmac=5agpVWsRchY0DObXs23vYWjjgqLZEBhqSvTwfCAcyng',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Onde?',
-				src: 'https://i.picsum.photos/id/650/200/200.jpg?hmac=gu3C13pBxCSHokbnumczMYlmWRLt3CFGx1sDaPpfRnk',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Quando?',
-				src: 'https://i.picsum.photos/id/565/200/200.jpg?hmac=QvKo8qgzFFNcZoXCpT0CNMDTwWd3ynwqLXxrzK2o8fw',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Como?',
-				src: 'https://i.picsum.photos/id/102/200/200.jpg?hmac=mLVQr0-wbZ-PRSf_R4oFkUDR7yfIKqT3F8pfJNFZKc0',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Qual?',
-				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Quantos?',
-				src: 'https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Que horas?',
-				src: 'https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8',
-				alt: 'Lorem Picsum',
-			},
-		],
-	},
-	{
-		id: 1635008786015,
-		order: 2,
-		name: 'Verbos I',
-		color: '#118ab2',
-		items: [
-			{
-				text: 'Estar',
-				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Querer',
-				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Precisar',
-				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Poder',
-				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Gostar',
-				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Saber',
-				src: 'https://i.picsum.photos/id/1080/200/200.jpg?hmac=0okKAdyiW9oTgR5PNZQrDYFtWu7HAt93nI93ZpfelUw',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Fazer',
-				src: 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Ler',
-				src: 'https://i.picsum.photos/id/638/200/200.jpg?hmac=64UpQ4ouFUNEG9cnXLQ9GxchDShg-mL1rdCrZGfc94U',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Tentar',
-				src: 'https://i.picsum.photos/id/268/200/200.jpg?hmac=I5JAWzISJc5x0jlDhEngvCIwyM0zxRh22iIIzHqOT80',
-				alt: 'Lorem Picsum',
-			},
-		],
-	},
-	{
-		id: 1635010742853,
-		order: 3,
-		name: 'Verbos II',
-		color: '#ef476f',
-		items: [
-			{
-				text: 'Comer',
-				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Beber',
-				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Ir',
-				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Vir',
-				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Sentir',
-				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
-				alt: 'Lorem Picsum',
-			},
-		],
-	},
-	{
-		id: 1635011064304,
-		order: 3,
-		name: 'Outros',
-		color: '#fb5607',
-		items: [
-			{
-				text: 'Pai',
-				src: 'https://i.picsum.photos/id/186/200/200.jpg?hmac=bNtKzMZT8HFzZq8mbTSWaQvmkX8T7TE47fspKMfxVl8',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Mãe',
-				src: 'https://i.picsum.photos/id/202/200/200.jpg?hmac=eGzhW5P2k0gzjc76Tk5T9lOfvn30h3YHuw5jGnBUY4Y',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Flamengo',
-				src: 'https://i.picsum.photos/id/612/200/200.jpg?hmac=HbIkwJ0QBqhSlGTi3bnF4JFTp9BntF-teQZUQhpqWyM',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Obrigado',
-				src: 'https://i.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
-				alt: 'Lorem Picsum',
-			},
-			{
-				text: 'Por favor',
-				src: 'https://i.picsum.photos/id/921/200/200.jpg?hmac=6pwJUhec4NqIAFxrha-8WXGa8yI1pJXKEYCWMSHroSU',
-				alt: 'Lorem Picsum',
-			},
-		],
-	},
+                text: "Lembrar",
+                src: "https://cdn-icons-png.flaticon.com/512/5692/5692232.png",
+				alt: "Flat Icon",
+            },
+        ],
+    },
+    {
+        id: 1635011064304,
+        order: 3,
+        name: "Outros",
+        color: "#fb5607",
+        items: [
+            {
+                text: "Pai",
+                src: "https://cdn-icons.flaticon.com/png/512/5021/premium/5021137.png?token=exp=1635186430~hmac=59d34c34dc36a92efeefdafe9963f36e",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Mãe",
+                src: "https://cdn-icons.flaticon.com/png/512/5021/premium/5021208.png?token=exp=1635186430~hmac=5d63d2bf3e0d85d385dc68d932b50c55",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Flamengo",
+                src: "https://cdn-icons-png.flaticon.com/512/861/861512.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Obrigado",
+                src: "https://cdn-icons-png.flaticon.com/512/417/417222.png",
+                alt: "Flat Icon",
+            },
+            {
+                text: "Por favor",
+                src: "https://cdn-icons.flaticon.com/png/512/4815/premium/4815743.png?token=exp=1635186722~hmac=a447d3b931b42b5e45399b3643ec2918",
+                alt: "Flat Icon",
+            },
+        ],
+    },
+    {
+        id: 1635184726990,
+        order: 4,
+        name: "Pessoas",
+        color: "#a8dadc",
+        items: [
+            {
+                text: "Professor",
+                src: "https://cdn-icons.flaticon.com/png/512/3153/premium/3153381.png?token=exp=1635185562~hmac=168e164707f0e7b5287f0ff7a9e89017",
+                alt: "Flat Icon",
+            },
+        ],
+    },
 ];
