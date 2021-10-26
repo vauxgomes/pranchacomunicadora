923dcfeb7f211b89e97d4e8c501c597ef97587da
vaux.gomes <vauxsandino@gmail.com>
2021-10-26 15:22:45 -0300

    - Configuration page: adding and modifying blocks

--- js/data.js
+++ js/data.js
@@ -1,46 +1,55 @@
-let data = [
+const DATA_NAME = "wellboard-data";
+const BASE_DATA = [
     {
         id: 1635008399217,
-        order: 0,
+        order: 1,
         name: "Pronomes",
         color: "#06d6a0",
         items: [
             {
+                order: 1,
                 text: "Eu",
                 src: "https://cdn-icons-png.flaticon.com/512/5737/5737070.png",
                 alt: "Flat Icon",
             },
             {
+                order: 2,
                 text: "Você",
                 src: "https://cdn-icons-png.flaticon.com/512/544/544507.png",
                 alt: "Flat Icon",
             },
             {
+                order: 3,
                 text: "Ele",
                 src: "https://cdn-icons-png.flaticon.com/512/5294/5294576.png",
                 alt: "Flat Icon",
             },
             {
+                order: 4,
                 text: "Ela",
                 src: "https://cdn-icons-png.flaticon.com/512/5294/5294584.png",
                 alt: "Flar Icon",
             },
             {
+                order: 5,
                 text: "Nós",
                 src: "https://cdn-icons-png.flaticon.com/512/5205/5205203.png",
                 alt: "Flat Icon",
             },
             {
+                order: 6,
                 text: "Isto",
                 src: "https://cdn-icons-png.flaticon.com/512/2235/2235335.png",
                 alt: "Flat Icon",
             },
             {
+                order: 7,
                 text: "Meu",
                 src: "https://cdn-icons-png.flaticon.com/512/5799/5799109.png",
                 alt: "Flat Icon",
             },
             {
+                order: 8,
                 text: "Seu",
                 src: "https://cdn-icons-png.flaticon.com/512/5799/5799098.png",
                 alt: "Flat Icon",
@@ -49,46 +58,54 @@ let data = [
     },
     {
         id: 1635008786015,
-        order: 1,
+        order: 2,
         name: "Perguntas",
         color: "#ffd166",
         items: [
             {
+                order: 1,
                 text: "O quê?",
                 src: "https://cdn-icons-png.flaticon.com/512/5485/5485820.png",
                 alt: "Flat Icon",
             },
             {
+                order: 2,
                 text: "Quem?",
                 src: "https://cdn-icons-png.flaticon.com/512/4784/4784321.png",
                 alt: "Flat Icon",
             },
             {
+                order: 3,
                 text: "Onde?",
                 src: "https://cdn-icons-png.flaticon.com/512/1527/1527531.png",
                 alt: "Flat Icon",
             },
             {
+                order: 4,
                 text: "Quando?",
                 src: "https://cdn-icons-png.flaticon.com/512/5247/5247244.png",
                 alt: "Flat Icon",
             },
             {
+                order: 5,
                 text: "Como?",
                 src: "https://cdn-icons-png.flaticon.com/512/5883/5883722.png",
                 alt: "Flat Icon",
             },
             {
+                order: 6,
                 text: "Qual?",
                 src: "https://cdn-icons-png.flaticon.com/512/1043/1043719.png",
                 alt: "Flat Icon",
             },
             {
+                order: 7,
                 text: "Quantos?",
                 src: "https://cdn-icons-png.flaticon.com/512/1039/1039337.png",
                 alt: "Flat Icon",
             },
             {
+                order: 8,
                 text: "Que horas?",
                 src: "https://cdn-icons-png.flaticon.com/512/5346/5346162.png",
                 alt: "Flat Icon",
@@ -97,51 +114,60 @@ let data = [
     },
     {
         id: 1635008786015,
-        order: 2,
+        order: 3,
         name: "Verbos I",
         color: "#118ab2",
         items: [
             {
+                order: 1,
                 text: "Estar",
                 src: "https://cdn-icons-png.flaticon.com/512/5737/5737025.png",
                 alt: "Flat Icon",
             },
             {
+                order: 2,
                 text: "Querer",
                 src: "https://cdn-icons-png.flaticon.com/512/4807/4807576.png",
                 alt: "Flat Icon",
             },
             {
+                order: 2,
                 text: "Ver",
                 src: "https://cdn-icons-png.flaticon.com/512/4062/4062968.png",
                 alt: "Flat Icon",
             },
             {
+                order: 3,
                 text: "Poder",
                 src: "https://cdn-icons-png.flaticon.com/512/5737/5737169.png",
                 alt: "Flat Icon",
             },
             {
+                order: 4,
                 text: "Gostar",
                 src: "https://cdn-icons-png.flaticon.com/512/5906/5906018.png",
                 alt: "Flat Icon",
             },
             {
+                order: 5,
                 text: "Saber",
                 src: "https://cdn-icons-png.flaticon.com/512/5201/5201967.png",
                 alt: "Flat Icon",
             },
             {
+                order: 6,
                 text: "Fazer",
                 src: "https://cdn-icons-png.flaticon.com/512/820/820723.png",
                 alt: "Flat Icon",
             },
             {
+                order: 7,
                 text: "Ler",
                 src: "https://cdn-icons-png.flaticon.com/512/3153/3153339.png",
                 alt: "Flat Icon",
             },
             {
+                order: 8,
                 text: "Tentar",
                 src: "https://cdn-icons-png.flaticon.com/512/5737/5737221.png",
                 alt: "Flat Icon",
@@ -150,46 +176,54 @@ let data = [
     },
     {
         id: 1635010742853,
-        order: 3,
+        order: 4,
         name: "Verbos II",
         color: "#ef476f",
         items: [
             {
+                order: 1,
                 text: "Comer",
                 src: "https://cdn-icons-png.flaticon.com/512/5757/5757041.png",
                 alt: "Flat Icon",
             },
             {
+                order: 2,
                 text: "Beber",
                 src: "https://cdn-icons-png.flaticon.com/512/5019/5019733.png",
                 alt: "Flat Icon",
             },
             {
+                order: 3,
                 text: "Ir",
                 src: "https://cdn-icons-png.flaticon.com/512/5019/5019577.png",
                 alt: "Flat Icon",
             },
             {
+                order: 4,
                 text: "Vir",
                 src: "https://cdn-icons-png.flaticon.com/512/1028/1028334.png",
                 alt: "Flat Icon",
             },
             {
+                order: 5,
                 text: "Sentir",
                 src: "https://cdn-icons-png.flaticon.com/512/5737/5737014.png",
                 alt: "Flat Icon",
             },
             {
+                order: 6,
                 text: "Estudar",
                 src: "https://cdn-icons-png.flaticon.com/512/2628/2628757.png",
                 alt: "Flat Icon",
             },
             {
+                order: 7,
                 text: "Passear",
                 src: "https://cdn-icons-png.flaticon.com/512/3837/3837708.png",
                 alt: "Flat Icon",
             },
             {
+                order: 8,
                 text: "Lembrar",
                 src: "https://cdn-icons-png.flaticon.com/512/5692/5692232.png",
                 alt: "Flat Icon",
@@ -198,31 +232,36 @@ let data = [
     },
     {
         id: 1635011064304,
-        order: 4,
+        order: 5,
         name: "Outros",
         color: "#fb5607",
         items: [
             {
+                order: 1,
                 text: "Pai",
                 src: "https://cdn-icons-png.flaticon.com/512/5098/5098004.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Mãe",
                 src: "https://cdn-icons-png.flaticon.com/512/5097/5097780.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Flamengo",
                 src: "https://cdn-icons-png.flaticon.com/512/861/861512.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Obrigado",
                 src: "https://cdn-icons-png.flaticon.com/512/417/417222.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Por favor",
                 src: "https://cdn-icons-png.flaticon.com/512/3445/3445913.png",
                 alt: "Flat Icon",
@@ -231,16 +270,18 @@ let data = [
     },
     {
         id: 1635184726990,
-        order: 5,
+        order: 6,
         name: "Pessoas",
         color: "#a8dadc",
         items: [
             {
+                order: 1,
                 text: "Professor",
                 src: "https://cdn-icons-png.flaticon.com/512/3723/3723284.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Amigos",
                 src: "https://cdn-icons-png.flaticon.com/512/1048/1048945.png",
                 alt: "Flat Icon",
@@ -249,21 +290,24 @@ let data = [
     },
     {
         id: 1635188319300,
-        order: 6,
+        order: 7,
         name: "Locais",
         color: "#EBC3DB",
         items: [
             {
+                order: 1,
                 text: "Quarto",
                 src: "https://cdn-icons-png.flaticon.com/512/1028/1028808.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Banheiro",
                 src: "https://cdn-icons-png.flaticon.com/512/1028/1028826.png",
                 alt: "Flat Icon",
             },
             {
+                order: 1,
                 text: "Cozinha",
                 src: "https://cdn-icons-png.flaticon.com/512/3059/3059052.png",
                 alt: "Flat Icon",
@@ -271,3 +315,10 @@ let data = [
         ],
     },
 ];
+
+let data = [];
+
+try {
+    data = JSON.parse(localStorage.getItem(DATA_NAME));
+    if (!data) data = BASE_DATA;
+} catch (error) {}
--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,25 @@
 function randomHexColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function isBright(color) {
-	const hex = color.replace('#', '');
+    const hex = color.replace("#", "");
 
-	const c_r = parseInt(hex.substr(0, 2), 16);
-	const c_g = parseInt(hex.substr(2, 2), 16);
-	const c_b = parseInt(hex.substr(4, 2), 16);
-	const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
+    const c_r = parseInt(hex.substr(0, 2), 16);
+    const c_g = parseInt(hex.substr(2, 2), 16);
+    const c_b = parseInt(hex.substr(4, 2), 16);
+    const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
 
-	return brightness > 155;
+    return brightness > 155;
+}
+
+function sortBlocks(blocks) {
+	console.log(blocks)
+    return blocks.sort((a, b) => {
+        if (a.order > b.order) return 1;
+        else if (a.order < b.order) return -1;
+        else if (a.name < b.name) return 1;
+        else if (a.name > b.name) return -1;
+        return a.id < b.id ? 1 : -1;
+    });
 }
